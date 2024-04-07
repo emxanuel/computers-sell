df71b4a4ac6e9c4177aab2206d17c5c061318b80
emxanuel <ealg0528@gmail.com>
2024-04-07 19:35:17 -0400

    update yml

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());    
 app.use((_: Request, res: Response, next: NextFunction) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', process.env.SITE);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
